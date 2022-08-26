7fb90349167c959f4f7fbd819f250d4b35d0018b
saleh alsaggaf <saa111954@hotmail.com>
2022-08-26 20:02:21 +0300

    update:update relations and documenting columns

--- src/app/cart/dto/update-cart.dto.ts
+++ src/app/cart/dto/update-cart.dto.ts
@@ -1,8 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsArray, IsOptional } from 'class-validator';
-import { Client } from 'src/app/client/entities/client.entity';
-import { Product } from 'src/app/product/entities/product.entity';
+import { IsArray, IsOptional } from 'class-validator';
+import { Client } from '../../client/entities/client.entity';
+import { Product } from '../../product/entities/product.entity';
 import { CreateCartDto } from './create-cart.dto';
 
 export class UpdateCartDto extends PartialType(CreateCartDto) {
